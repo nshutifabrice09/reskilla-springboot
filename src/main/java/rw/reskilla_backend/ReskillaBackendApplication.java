b45016e18152e52587cd175b166c984fc7e4bccb
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-23 10:08:02 +0200

    initial commit

---
+++ src/main/java/rw/reskilla_backend/ReskillaBackendApplication.java
@@ -0,0 +1,13 @@
+package rw.reskilla_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReskillaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReskillaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/reskilla_backend/ReskillaBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package rw.reskilla_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReskillaBackendApplicationTests {
+
+	
+
+	@Test
+	void contextLoads() {
+	}
+
+}
