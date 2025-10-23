c8186548d5f2caf994dffe39f13f91dc0379ad11
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-23 10:13:49 +0200

    second commit

--- src/main/java/rw/reskilla_backend/ReskillaBackendApplication.java
+++ src/main/java/rw/reskilla_backend/ReskillaBackendApplication.java
@@ -2,10 +2,17 @@ package rw.reskilla_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ReskillaBackendApplication {
 
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to CODM";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ReskillaBackendApplication.class, args);
 	}
