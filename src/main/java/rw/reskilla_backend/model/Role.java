0754a563f1ce034649d232de4bccd9cecd68d511
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-24 10:00:26 +0200

    model commit

---
+++ src/main/java/rw/reskilla_backend/model/Role.java
@@ -0,0 +1,5 @@
+package rw.reskilla_backend.model;
+
+public enum Role {
+    LEARNER, INSTRUCTOR, ADMIN
+}
---
+++ src/main/java/rw/reskilla_backend/model/User.java
@@ -0,0 +1,112 @@
+package rw.reskilla_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private boolean active = true;
+
+    private String profilePicture;
+
+    private String currentOccupation;
+
+    private String careerGoal;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getCurrentOccupation() {
+        return currentOccupation;
+    }
+
+    public void setCurrentOccupation(String currentOccupation) {
+        this.currentOccupation = currentOccupation;
+    }
+
+    public String getCareerGoal() {
+        return careerGoal;
+    }
+
+    public void setCareerGoal(String careerGoal) {
+        this.careerGoal = careerGoal;
+    }
+}
